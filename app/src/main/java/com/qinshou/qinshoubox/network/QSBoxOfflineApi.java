ff2af0870ab8263add1cf6eefdd6b7200a48370c
mrqinshou <cqflqinhao@126.com>
2019-11-24 22:51:53 +0800

    Feat:1.增加获取离线消息的接口.

---
+++ app/src/main/java/com/qinshou/qinshoubox/network/QSBoxOfflineApi.java
@@ -0,0 +1,30 @@
+package com.qinshou.qinshoubox.network;
+
+import com.qinshou.immodule.bean.GroupChatBean;
+import com.qinshou.immodule.bean.MessageBean;
+import com.qinshou.okhttphelper.annotation.Api;
+import com.qinshou.okhttphelper.annotation.DefaultHost;
+import com.qinshou.okhttphelper.annotation.Field;
+import com.qinshou.okhttphelper.annotation.Json;
+import com.qinshou.okhttphelper.annotation.Post;
+import com.qinshou.okhttphelper.call.Call;
+import com.qinshou.qinshoubox.homepage.bean.QinshouResultBean;
+
+import java.util.List;
+
+/**
+ * Author: QinHao
+ * Email:qinhao@jeejio.com
+ * Date: 2019/11/22 13:35
+ * Description:QSBox 离线数据模块的接口
+ */
+@Api
+public interface QSBoxOfflineApi {
+    @DefaultHost
+//    String DEFAULT_HOST = "http://172.16.60.231:8080/groupChat/";
+    String DEFAULT_HOST = "http://192.168.1.109:8080/offline";
+
+    @Json
+    @Post("/getOfflineMessageList")
+    Call<QinshouResultBean<List<MessageBean>>> getOfflineMessageList(@Field(name = "toUserId") int toUserId);
+}
--- app/src/main/java/com/qinshou/qinshoubox/util/userstatusmanager/LogoutStatus.java
+++ app/src/main/java/com/qinshou/qinshoubox/util/userstatusmanager/LogoutStatus.java
@@ -5,15 +5,22 @@ import android.content.Intent;
 
 import com.qinshou.commonmodule.ContainerActivity;
 import com.qinshou.commonmodule.util.SharedPreferencesHelper;
+import com.qinshou.commonmodule.util.ShowLogUtil;
+import com.qinshou.immodule.bean.MessageBean;
 import com.qinshou.immodule.manager.ChatManager;
+import com.qinshou.okhttphelper.callback.Callback;
 import com.qinshou.qinshoubox.MainActivity;
 import com.qinshou.qinshoubox.constant.IConstant;
 import com.qinshou.immodule.db.DBHelper;
 import com.qinshou.qinshoubox.friend.view.fragment.FriendFragment;
 import com.qinshou.immodule.bean.UserBean;
 import com.qinshou.qinshoubox.me.ui.fragment.LoginOrRegisterFragment;
+import com.qinshou.qinshoubox.network.OkHttpHelperForQSBoxOfflineApi;
+import com.qinshou.qinshoubox.transformer.QSApiTransformer;
 
 import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
 
 
 /**
@@ -42,6 +49,19 @@ public class LogoutStatus implements IUserStatus {
         EventBus.getDefault().post(userBean);
         // 更新好友申请历史数
         EventBus.getDefault().post(0);
+        OkHttpHelperForQSBoxOfflineApi.SINGLETON.getOfflineMessageList(userBean.getId())
+                .transform(new QSApiTransformer<List<MessageBean>>())
+                .enqueue(new Callback<List<MessageBean>>() {
+                    @Override
+                    public void onSuccess(List<MessageBean> data) {
+                        ShowLogUtil.logi(data);
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        ShowLogUtil.logi("onFailure: e--->" + e.getMessage());
+                    }
+                });
     }
 
     @Override
