65e395aa28f973ee58f95cf10b258b220dc45be1
qinhao <qinhao@jeejio.com>
2020-05-18 14:06:17 +0800

    Feat:1.优化代码格式.

--- app/src/main/java/com/qinshou/qinshoubox/me/ui/dialog/BusinessManFloor12Dialog.java
+++ app/src/main/java/com/qinshou/qinshoubox/me/ui/dialog/BusinessManFloor12Dialog.java
@@ -18,42 +18,6 @@ import com.qinshou.qinshoubox.util.MagicGameManager;
 
 public class BusinessManFloor12Dialog extends AbsDialogFragment {
     private RadioGroup mRadioGroup;
-    //    @Override
-//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        //设置对话框无标题
-//        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
-//
-//        //设置对话框自带背景透明,才能显示布局的背景
-//        final Window mWindow = getDialog().getWindow();
-//        if (mWindow != null) {
-//            mWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-//        }
-//        //设置点击外部不可消失
-//        getDialog().setCanceledOnTouchOutside(false);
-//
-//        View rootView = inflater.inflate(R.layout.dialog_business_man_floor_12, null);
-//        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
-//        rootView.findViewById(R.id.btn_confirm).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                switch (radioGroup.getCheckedRadioButtonId()) {
-//                    case R.id.radio_button_1:
-//                        warriorBean.sellYellowKeyFor7Money();
-//                        break;
-//                    case R.id.radio_button_2:
-//                        warriorBean.sellBlueKeyFor35Money();
-//                        break;
-//                    case R.id.radio_button_3:
-//                        warriorBean.sellRedKeyFor70Money();
-//                        break;
-//                    case R.id.radio_button_4:
-//                        dismiss();
-//                        break;
-//                }
-//            }
-//        });
-//        return rootView;
-//    }
 
     @Override
     public int initLayoutId() {
